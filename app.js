93f5f05d2b1f3dda0935cea2ba14ce0e91c3c0de
GarethW1994 <williams.gareth0@gmail.com>
2016-10-06 11:29:03 +0200

    Completed taxi move and light exercise

--- app.js
+++ app.js
@@ -4,29 +4,81 @@ var body = document.querySelector("body");
 
 var taxiPositionCounter = 1;
 
+
 body.onkeydown = function(e){
+
+  console.log(e);
+
+    displayTaxiPassengerCount(7);
 
     //right arrow : 39
     //left arrow  : 37
     //up arrow    : 38
     //down arrow  : 40
 
-    displayMessage(e.keyCode);
+    var myTrafficLight = new TrafficLight(taxiPositionCounter);
+
+
 
     // to move the taxi forward...
     // when the right arrow is pressed
     //work with `taxiPositionCounter`
+    if (myTrafficLight.state() === 'orange') {
+      displayMessage('Traffic Light is orange. Slow down!');
+    }
+    if (e.keyCode === 38) {
+    if (myTrafficLight.state() === 'green' || myTrafficLight.state() === 'orange') {
+      myTrafficLight.red();
+      displayMessage("Traffic Light Red. Switch light to green to move!")
+  }
+
+  }
+
+  if (e.keyCode === 40) {
+    if (myTrafficLight.state() === 'red' || myTrafficLight.state() === 'orange') {
+      myTrafficLight.green();
+      displayMessage('Traffic Light is green. You may go!')
+    }
+
+    }
+
 
     // call `calculateTaxiLocation` to get a taxi location className store it in a variable.
     // increment the `taxiPositionCounter`
     // call `calculateTaxiLocation` to the new a taxi location className store it in another variable.
-
-    //pass both to the `moveTaxi( firstClassName, secondClassName)` function
-    //
-
-    var tl = new TrafficLight(2);
-    
-    tl.orange();
+    //Declare new traffic light
 
 
-};
+    if (e.keyCode === 39){
+      if (myTrafficLight.state() === 'red') {
+          //
+      }
+      else {
+        var taxiLocation = calculateTaxiLocation(taxiPositionCounter);
+        taxiPositionCounter++;
+        var currentLocation = calculateTaxiLocation(taxiPositionCounter);
+        //pass both to the `moveTaxi( firstClassName, secondClassName)` function
+        //
+        moveTaxi(taxiLocation, currentLocation);
+
+      }
+
+}
+
+//move taxi backwards
+  if (e.keyCode === 37) {
+    if (myTrafficLight.state() === 'red') {
+      //
+    }
+
+    else {var taxiLocation = calculateTaxiLocation(taxiPositionCounter);
+    taxiPositionCounter--;
+    var currentLocation = calculateTaxiLocation(taxiPositionCounter);
+    moveTaxi(taxiLocation, currentLocation);
+  }
+}
+
+
+
+
+}
